e78e52a3c721f6cc3112720b4963be5ca571a088
Diazyanita <dzzynt0@gmail.com>
2024-08-07 11:17:39 +0700

    first commit

---
+++ fungsi.py
@@ -0,0 +1,8 @@
+from tgs2 import book
+
+
+file = book()
+
+file.baca_file('book.txt')
+
+file.tulis_file('book.txt', 'a')
---
+++ tgs2.py
@@ -0,0 +1,26 @@
+class book:
+    def __init__(self):
+        pass
+
+    def baca_file(self,nama_file):
+        try:
+            with open(nama_file,'r')as file:
+                isi_file = file.read()
+                print(isi_file)
+        except FileNotFoundError:
+            print(f"file {nama_file} tidak ada.")
+        except Exception as e:
+            print(f"Terjadi Kesalahan saat membaca: {e}")
+
+    def tulis_file(swlft, nama_file, mode):
+        try:
+           nama_buku = input ("judul buku: ")
+           teks = "\n {}". format(nama_buku)
+           with open (nama_file, mode) as file:
+               file.write(teks)
+               print(f"Teks sudah di tulis & di simpan {nama_file}")
+        except Exception as e:
+            print(f"Gagal menulis: {e}")
+
+
+
